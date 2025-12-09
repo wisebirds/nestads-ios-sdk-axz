015f27a3343e879c28e523dbf7ed07134e2ece98
kangwook <kngwk.bsns@wisebirds.com>
2025-12-09 20:20:09 +0900

    Initial Commit

---
+++ Tests/nestads-ios-sdk-axzTests/nestads_ios_sdk_axzTests.swift
@@ -0,0 +1,12 @@
+import XCTest
+@testable import nestads_ios_sdk_axz
+
+final class nestads_ios_sdk_axzTests: XCTestCase {
+    func testExample() throws {
+        // XCTest Documentation
+        // https://developer.apple.com/documentation/xctest
+
+        // Defining Test Cases and Test Methods
+        // https://developer.apple.com/documentation/xctest/defining_test_cases_and_test_methods
+    }
+}
